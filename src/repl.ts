6eb37ea4e57dcc14045114cc049d9e4fe6524df4
gosho-kazuya <ketsume0211@gmail.com>
2018-12-05 12:30:55 +0900

    remove unused

--- src/repl.ts
+++ src/repl.ts
@@ -36,7 +36,7 @@ export const start = () => {
   process.stdin.on('keypress', onKeyPress)
 }
 
-const onKeyPress = async (str: any, key: KeyInfo) => {
+export const onKeyPress = async (_str: any, key: KeyInfo) => {
   if (key.ctrl) {
     switch (key.name) {
       case 'a':
@@ -66,17 +66,11 @@ const onKeyPress = async (str: any, key: KeyInfo) => {
       await actions.runCommand(ctx)
       return
     default:
-      return actions.input(str)
+      return actions.input(key.sequence)
   }
 }
 
 // export default class Repl {
-//   replCount: number = 1
-//   history: string[] = []
-//   historyIndex: number = 0
-//   tty: Tty
-//   ctx: Context
-//
 //   showCompletionCandidates() {
 //     const { localVars, omniCompletions, search } = complete(
 //       this.ctx,
@@ -103,96 +97,5 @@ const onKeyPress = async (str: any, key: KeyInfo) => {
 //     if (key.name === 'tab') {
 //       return this.showCompletionCandidates()
 //     }
-//
-//     if (key.name === 'backspace') {
-//       if (this.tty.cursorPosition === 0) {
-//         return
-//       }
-//       const [before, after] = this.tty.splitCommandAtCursor()
-//       const position = this.tty.cursorPosition
-//       this.tty.setCommand(before.slice(0, -1) + after)
-//       this.tty.cursorPosition = position - 1
-//       this.tty.setPosition(position - 1)
-//       return
-//     }
-//
-//     if (key.name === 'return') {
-//
-//       if (this.tty.command === 'ls') {
-//         const res = this.showCompletionCandidates()
-//         this.tty.setCommand('')
-//         this.replCount++
-//         this.tty.setPrompt(this.prompt)
-//         this.tty.cursorPosition = 0
-//         return res
-//       }
-//
-//       if (this.history.slice(-1)[0] !== this.tty.command) {
-//         this.history.push(this.tty.command)
-//       }
-//       this.historyIndex = this.history.length
-//       this.tty.cursorPosition = 0
-//       console.log()
-//       let res: any
-//     }
-//
-//     if (key.ctrl && key.name === 'a') {
-//       this.tty.setPosition(0)
-//       return
-//     }
-//
-//     if (key.ctrl && key.name === 'e') {
-//       this.tty.setPosition(this.tty.command.length)
-//       return
-//     }
-//
-//     if (key.ctrl && key.name === 'b') {
-//       if (this.tty.cursorPosition === 0) {
-//         return
-//       }
-//       this.tty.moveCursor(-1)
-//       return
-//     }
-//
-//     if (key.ctrl && key.name === 'f') {
-//       store.dispatch({
-//         type: types.CURSOR_RIGHT,
-//       })
-//       if (this.tty.cursorPosition >= this.tty.command.length) {
-//         return
-//       }
-//       this.tty.moveCursor(1)
-//       return
-//     }
-//
-//     if (key.ctrl && key.name === 'p') {
-//       if (this.historyIndex === 0 || this.history.length === 0) {
-//         return
-//       }
-//       this.historyIndex--
-//       const command = this.history[this.historyIndex]
-//       if (!command) {
-//         return
-//       }
-//       this.tty.setPosition(command.length)
-//       this.tty.setCommand(command)
-//       return
-//     }
-//
-//     if (key.ctrl && key.name === 'n') {
-//       if (this.historyIndex === this.history.length) {
-//         return
-//       }
-//       this.historyIndex++
-//       const command = this.history[this.historyIndex]
-//       if (command) {
-//         this.tty.setCommand(command)
-//       } else {
-//         this.tty.setCommand('')
-//       }
-//       return
-//     }
-//
-//     this.tty.insert(key.sequence)
 //   }
 // }
